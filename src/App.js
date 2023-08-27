03d792ed8bf2ec555208515fa598459683932696
vinod859 <vk6560920@gmail.com>
2023-08-27 17:38:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <h1>E-COMM PROJECT</h1>
+      <button>normal button</button>
+      <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
